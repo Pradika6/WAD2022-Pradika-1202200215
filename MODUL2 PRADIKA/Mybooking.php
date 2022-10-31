8df38b9501fe58c5b92714e2ca3328ffa09ff05c
arifraihana01 <arif.raihannabil90@gmail.com>
2022-10-31 08:03:37 +0700

    update

--- MODUL2 PRADIKA/Mybooking.php
+++ MODUL2 PRADIKA/Mybooking.php
@@ -1,99 +1,99 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <title>MY BOOKING</title>
-        <?php
-            $nama = "Pradika_1202200215";
-            date_default_timezone_set('GMT');
-            $tgl = $_POST['date'];
-            $time = $_POST['time'];
-            $durasi = $_POST['durasi'];
-            $ci = date("d-m-Y H:i", strtotime("$tgl $time"));
-            $co = date('d-m-y H:i', strtotime($durasi . " " . "days", strtotime($ci)));
-            $type = $_POST['car'];
-            $hp = $_POST['nohp'];
-            $total = 0;
-            $type = $_POST['car'];
-            if ($type == 'Toyota Rush') {
-                $total += (200000*$durasi);
-            }else if($type == 'Toyota Ayla'){
-                $total += (150000*$durasi);
-            }elseif ($type == 'Honda Brio') {
-                $total += (150000*$durasi);
-            }
-            $servis = $_POST['servis'];
-            if($servis == 'health'){
-                $total += 25000;
-            }
-            elseif($servis == 'driver'){
-                $total += 100000;
-            }
-            elseif ($servis == 'fuel') {
-                $total += 250000;
-            }
-        ?>
-    </head>
-    <body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-center align-item-center" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link" aria-current="page" href="home.php">Home</a>
-                        <a class="nav-link" href="booking.php">Booking</a>
+    <!doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <title>MY BOOKING</title>
+            <?php
+                $nama = "Pradika_1202200215";
+                date_default_timezone_set('GMT');
+                $tgl = $_POST['date'];
+                $time = $_POST['time'];
+                $durasi = $_POST['durasi'];
+                $ci = date("d-m-Y H:i", strtotime("$t9l $time"));
+                $co = date('d-m-y H:i', strtotime($durasi . " " . "days", strtotime($ci)));
+                $type = $_POST['car'];
+                $hp = $_POST['nohp'];
+                $total = 0;
+                $type = $_POST['car'];
+                if ($type == 'Toyota Rush') {
+                    $total += (200000*$durasi);
+                }else if($type == 'Toyota Ayla'){
+                    $total += (150000*$durasi);
+                }elseif ($type == 'Honda Brio') {
+                    $total += (150000*$durasi);
+                }
+                $servis = $_POST['servis'];
+                if($servis == 'health'){
+                    $total += 25000;
+                }
+                elseif($servis == 'driver'){
+                    $total += 100000;
+                }
+                elseif ($servis == 'fuel') {
+                    $total += 250000;
+                }
+            ?>
+        </head>
+        <body>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-center align-item-center" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-link" aria-current="page" href="home.php">Home</a>
+                            <a class="nav-link" href="booking.php">Booking</a>
+                        </div>
                     </div>
                 </div>
+            </nav>
+            <div class="pt-5">
+                <h4 class="text-center pt-5">Thank you <?= $nama ?> for reserving</h4>
+                <h5 class="text-center">Please double check your reservations details</h5>
+                <div class="container-fluid">
+                    <table class="table">
+                        <tr>
+                            <th scope="col">Booking Number</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Check-in</th>
+                            <th scope="col">Check-out</th>
+                            <th scope="col">Car Type</th>
+                            <th scope="col">Phone Number</th>
+                            <th scope="col">Service</th>
+                            <th scope="col">Total Price</th>
+                        </tr>
+                        <tr class="table-light">
+                            <th scope="row"><?= rand(100000000, 999999999); ?></th>
+                            <td><?= $nama ?></td>
+                            <td><?= $ci ?></td>
+                            <td><?= $co ?></td>
+                            <td><?= $type ?></td>
+                            <td><?= $hp ?></td>
+                            <td>
+                                <ul>
+                                    <?php
+                                    if(isset($_POST['servis'])){
+                                        // foreach ($_POST['servis'] as $servis) {
+                                            echo "<li>$servis</li>";
+                                        // }
+                                    }
+                                    else{
+                                        echo "No Service";
+                                    }
+                                    ?>
+                                </ul>
+                            </td>
+                            <td>Rp. <?= $total ?></td>
+                        </tr>
+                    </table>
+                </div>
             </div>
-        </nav>
-        <div class="pt-5">
-            <h4 class="text-center pt-5">Thank you <?= $nama ?> for reserving</h4>
-            <h5 class="text-center">Please double check your reservations details</h5>
-            <div class="container-fluid">
-                <table class="table">
-                    <tr>
-                        <th scope="col">Booking Number</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Check-in</th>
-                        <th scope="col">Check-out</th>
-                        <th scope="col">Car Type</th>
-                        <th scope="col">Phone Number</th>
-                        <th scope="col">Service</th>
-                        <th scope="col">Total Price</th>
-                    </tr>
-                    <tr class="table-light">
-                        <th scope="row"><?= rand(100000000, 999999999); ?></th>
-                        <td><?= $nama ?></td>
-                        <td><?= $ci ?></td>
-                        <td><?= $co ?></td>
-                        <td><?= $type ?></td>
-                        <td><?= $hp ?></td>
-                        <td>
-                            <ul>
-                                <?php
-                                if(isset($_POST['servis'])){
-                                    // foreach ($_POST['servis'] as $servis) {
-                                        echo "<li>$servis</li>";
-                                    // }
-                                }
-                                else{
-                                    echo "No Service";
-                                }
-                                ?>
-                            </ul>
-                        </td>
-                        <td>Rp. <?= $total ?></td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    <br><br><br><br><br><br><br><br><br><br>
-        <footer>
-           <p class="text-center pt-2 mb-2 py-1" style=" background-color: #ddd;">Created by PRADIKA_1202200215</p>
-        </footer>
-    </body>
-</html>
+        <br><br><br><br><br><br><br><br><br><br>
+            <footer>
+            <p class="text-center pt-2 mb-2 py-1" style=" background-color: #ddd;">Created by PRADIKA_1202200215</p>
+            </footer>
+        </body>
+    </html>
--- MODUL2 PRADIKA/home.php
+++ MODUL2 PRADIKA/home.php
@@ -1,108 +1,108 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <title>HOME</title>
-    </head>
-    <body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-center align-item-center" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link" aria-current="page" href="home.php">Home</a>
-                        <a class="nav-link" href="booking.php">Booking</a>
+    <!doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <title>HOME</title>
+        </head>
+        <body>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-center align-item-center" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-link" aria-current="page" href="home.php">Home</a>
+                            <a class="nav-link" href="booking.php">Booking</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <div class="pt-5">
+                <h4 class="text-center pt-4">WELCOME TO EAD RENT</h4>
+                <div class="container pt-2">
+                    <h5 class="text-center" style="padding: 1px; font-family: sans-serif; font-size: 18px;">Find your best deal, here!</h5>
+                    <div class="row pt-3">
+                        <div class="col-md d-flex justify-content-center">
+                            <form method="post" action="booking.php">
+                                <div class="card">
+                                    <img src="https://www.toyota.astra.co.id/sites/default/files/2021-08/1-white.png" class="card-img-top"  style="height :200px" alt="Ayla">
+                                    <input name="image" value="https://www.toyota.astra.co.id/sites/default/files/2021-08/1-white.png" hidden>
+                                    <div class="card-body">
+                                        <h5 class="card-title" style="font-weight:bold">Toyota Rush</h5>
+                                        <input name="car" value="Toyota Rush" hidden>
+                                        <div class="card-text">
+                                            <h6 class="text-secondary">Rp. 200.000 / Day</h6>
+                                            <input name="price" value="Rp. 200000" hidden>
+                                        </div>
+                                    </div>
+                                    <ul class="list-group list-group-flush text-center">
+                                        <li class="list-group-item text-primary" style="font-weight:bold">7 Kursi</li>
+                                        <li class="list-group-item text-primary" style="font-weight:bold">1500 CC</li>
+                                        <li class="list-group-item text-primary" style="font-weight:bold">Manual</li>
+                                        <li class="list-group-item bg-light">
+                                            <input class="btn btn-primary" type="submit" name="submit" value="Book Now">
+                                        </li>
+                                    </ul>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col-md d-flex justify-content-center">
+                            <form method="post" action="booking.php">
+                                <div class="card">
+                                    <img src="https://www.bandung-daihatsu.com/wp-content/uploads/2022/07/Daihatsu-Ayla-2022.webp" class="card-img-top"  style="height :200px" alt="Rush">
+                                    <input name="image" value="https://www.bandung-daihatsu.com/wp-content/uploads/2022/07/Daihatsu-Ayla-2022.webp" hidden>
+                                    <div class="card-body">
+                                        <h5 class="card-title" style="font-weight:bold">Toyota Ayla</h5>
+                                        <input name="car" value="Toyota Ayla" hidden>
+                                        <div class="card-text">
+                                            <h6 class="text-secondary">Rp. 150.000 / Day</h6>
+                                            <input name="price" value="Rp. 150000" hidden>
+                                        </div>
+                                    </div>
+                                    <ul class="list-group list-group-flush text-center">
+                                        <li class="list-group-item text-primary" style="font-weight:bold">5 Kursi</li>
+                                        <li class="list-group-item text-primary" style="font-weight:bold">1200 CC</li>
+                                        <li class="list-group-item text-primary" style="font-weight:bold">Manual</li>
+                                        <li class="list-group-item bg-light">
+                                            <input class="btn btn-primary" type="submit" name="submit" value="Book Now">
+                                        </li>
+                                    </ul>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col-md d-flex justify-content-center">
+                            <form method="post" action="booking.php">
+                                <div class="card">
+                                    <img src="https://asset.kompas.com/crops/6gVJpIv3YYVUH1pN4JZIJqfQlZQ=/0x461:1280x1314/750x500/data/photo/2022/05/13/627e75b39d8d4.png" class="card-img-top"  style="height :200px" alt="Brio">
+                                    <input name="image" value="https://asset.kompas.com/crops/6gVJpIv3YYVUH1pN4JZIJqfQlZQ=/0x461:1280x1314/750x500/data/photo/2022/05/13/627e75b39d8d4.png" hidden>
+                                    <div class="card-body">
+                                        <h5 class="card-title" style="font-weight:bold">Honda Brio</h5>
+                                        <input name="car" value="Honda Brio" hidden>
+                                        <div class="card-text">
+                                            <h6 class="text-secondary">Rp. 150.000 / Day</h6>
+                                            <input name="price" value="Rp. 150000" hidden>
+                                        </div>
+                                    </div>
+                                    <ul class="list-group list-group-flush text-center">
+                                        <li class="list-group-item text-primary" style="font-weight:bold">5 Kursi</li>
+                                        <li class="list-group-item text-primary" style="font-weight:bold">1200 CC</li>
+                                        <li class="list-group-item text-primary" style="font-weight:bold">Automatic</li>
+                                        <li class="list-group-item bg-light">
+                                            <input class="btn btn-primary" type="submit" name="submit" value="Book Now">
+                                        </li>
+                                    </ul>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </nav>
-        <div class="pt-5">
-            <h4 class="text-center pt-4">WELCOME TO EAD RENT</h4>
-            <div class="container pt-2">
-                <h5 class="text-center" style="padding: 1px; font-family: sans-serif; font-size: 18px;">Find your best deal, here!</h5>
-                <div class="row pt-3">
-                    <div class="col-md d-flex justify-content-center">
-                        <form method="post" action="booking.php">
-                            <div class="card">
-                                <img src="https://www.toyota.astra.co.id/sites/default/files/2021-08/1-white.png" class="card-img-top"  style="height :200px" alt="Ayla">
-                                <input name="image" value="https://www.toyota.astra.co.id/sites/default/files/2021-08/1-white.png" hidden>
-                                <div class="card-body">
-                                    <h5 class="card-title" style="font-weight:bold">Toyota Rush</h5>
-                                    <input name="car" value="Toyota Rush" hidden>
-                                    <div class="card-text">
-                                        <h6 class="text-secondary">Rp. 200.000 / Day</h6>
-                                        <input name="price" value="Rp. 200000" hidden>
-                                    </div>
-                                </div>
-                                <ul class="list-group list-group-flush text-center">
-                                    <li class="list-group-item text-primary" style="font-weight:bold">7 Kursi</li>
-                                    <li class="list-group-item text-primary" style="font-weight:bold">1500 CC</li>
-                                    <li class="list-group-item text-primary" style="font-weight:bold">Manual</li>
-                                    <li class="list-group-item bg-light">
-                                        <input class="btn btn-primary" type="submit" name="submit" value="Book Now">
-                                    </li>
-                                </ul>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="col-md d-flex justify-content-center">
-                        <form method="post" action="booking.php">
-                            <div class="card">
-                                <img src="https://www.bandung-daihatsu.com/wp-content/uploads/2022/07/Daihatsu-Ayla-2022.webp" class="card-img-top"  style="height :200px" alt="Rush">
-                                <input name="image" value="https://www.bandung-daihatsu.com/wp-content/uploads/2022/07/Daihatsu-Ayla-2022.webp" hidden>
-                                <div class="card-body">
-                                    <h5 class="card-title" style="font-weight:bold">Toyota Ayla</h5>
-                                    <input name="car" value="Toyota Ayla" hidden>
-                                    <div class="card-text">
-                                        <h6 class="text-secondary">Rp. 150.000 / Day</h6>
-                                        <input name="price" value="Rp. 150000" hidden>
-                                    </div>
-                                </div>
-                                <ul class="list-group list-group-flush text-center">
-                                    <li class="list-group-item text-primary" style="font-weight:bold">5 Kursi</li>
-                                    <li class="list-group-item text-primary" style="font-weight:bold">1200 CC</li>
-                                    <li class="list-group-item text-primary" style="font-weight:bold">Manual</li>
-                                    <li class="list-group-item bg-light">
-                                        <input class="btn btn-primary" type="submit" name="submit" value="Book Now">
-                                    </li>
-                                </ul>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="col-md d-flex justify-content-center">
-                        <form method="post" action="booking.php">
-                            <div class="card">
-                                <img src="https://asset.kompas.com/crops/6gVJpIv3YYVUH1pN4JZIJqfQlZQ=/0x461:1280x1314/750x500/data/photo/2022/05/13/627e75b39d8d4.png" class="card-img-top"  style="height :200px" alt="Brio">
-                                <input name="image" value="https://asset.kompas.com/crops/6gVJpIv3YYVUH1pN4JZIJqfQlZQ=/0x461:1280x1314/750x500/data/photo/2022/05/13/627e75b39d8d4.png" hidden>
-                                <div class="card-body">
-                                    <h5 class="card-title" style="font-weight:bold">Honda Brio</h5>
-                                    <input name="car" value="Honda Brio" hidden>
-                                    <div class="card-text">
-                                        <h6 class="text-secondary">Rp. 150.000 / Day</h6>
-                                        <input name="price" value="Rp. 150000" hidden>
-                                    </div>
-                                </div>
-                                <ul class="list-group list-group-flush text-center">
-                                    <li class="list-group-item text-primary" style="font-weight:bold">5 Kursi</li>
-                                    <li class="list-group-item text-primary" style="font-weight:bold">1200 CC</li>
-                                    <li class="list-group-item text-primary" style="font-weight:bold">Automatic</li>
-                                    <li class="list-group-item bg-light">
-                                        <input class="btn btn-primary" type="submit" name="submit" value="Book Now">
-                                    </li>
-                                </ul>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    
-        <footer>
-            <p class="text-center pt-2 mt-5 py-1" style=" background-color: #ddd;">Created by PRADIKA_1202200215</p>
-        </footer>
-    </body>
-</html>
+        
+            <footer>
+                <p class="text-center pt-2 mt-5 py-1" style=" background-color: #ddd;">Created by PRADIKA_1202200215</p>
+            </footer>
+        </body>
+    </html>
