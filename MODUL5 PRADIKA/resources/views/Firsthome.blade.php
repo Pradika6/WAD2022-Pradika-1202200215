b26b1b9f6c9614e4db8e3b4ea7a78d58c6e08bd2
arifraihana01 <arif.raihannabil90@gmail.com>
2022-12-11 22:00:49 +0700

    a

---
+++ MODUL5 PRADIKA/resources/views/Firsthome.blade.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Home Awal</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container mt-2 mb-2">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link active" aria-current="page" href="">Home</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto my-4 my-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/login">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div>
+        <div class="container mt-3 pt-5 ">
+            <div class="row clearfix" style="margin-top: 100px;">
+                <div class="card border-0" style="width: 600px; height: 700px;">
+                    <div class="deals-block-one">
+                        <div class="inner-box">
+                            <div class="lower-content">
+                            <br><br><br><br><br>
+                                <div class="tittle-text">
+                                    <h1>SELAMAT DATANG DI SHOWROOM</h1>
+                                </div>
+                                <p class="lead">Showroom Khusus Mobil BMW Terlengkap, Termurah di Kota Bandung</p>
+                                <nav class="navbar navbar-expand-xl navbar-light sticky-top" style="fill-opacity: 0;">
+                                    <div class="collapse navbar-collapse" id = "main-nav">
+                                        <ul class="navbar-nav mt-5">
+                                            <li class="nav-item">
+                                                <img src="https://drive.google.com/uc?export=view&id=1hqBNDU1Tx1RKd8wzC1bmnhwBr-7YsK23" alt="logo" width="100px">
+                                            <li class="nav-item mx-3">
+                                                <p class="text-center" data-bs-toggle = "modal" data-bs-target= "#crpradika" >Pradika_1202200215</p>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card border-0" style="width: 500px; height: 500px;">
+                    <div>
+                        <figure class="image"><img src="https://cdn.bmwblog.com/wp-content/uploads/2021/02/BMW-M-Sikora-showroom-in-Poland-1.jpg" width="700" height="500"></figure> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MODUL5 PRADIKA/resources/views/Register.blade.php
@@ -0,0 +1,110 @@
+<?php
+
+if (isset($_POST["daftar"])){
+    
+    if(registrasi($_POST)>0){
+        echo "<script>
+                alert('akun berhasil di daftarkan!'); 
+            </script>";
+    }else
+    echo mysqli_error($connect);
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Register</title>
+  </head>
+  <body>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div class="container mt-2 mb-2">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" aria-current="page" href="/">Home</a>
+                        </li>
+                    </ul>
+                    <div 
+                    class="collapse navbar-collapse" 
+                    id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item active">
+                        <a href="/login" class="nav-link">
+                                Login
+                            </a>
+                        </li>
+                        <li class="nav-item active">
+                            <a href="/register" class="nav-link active">
+                                Register
+                            </a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    <!-- End Navbar -->
+    <!-- Card Login -->
+    <section class="vh-100" style="background-color: #0000;">
+  <div class="container py-0 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-xl-10">
+        <div class="card" style="border-radius: 1rem; background-color: #eee">
+          <div class="row g-0">
+            <div class="col-md-6 col-lg-5 d-none d-md-block">
+              <img src="https://mediapool.bmwgroup.com/cache/P9/201712/P90289855/P90289855-the-world-s-first-bmw-m-and-bmw-m-performance-showroom-in-singapore-12-2017-2249px.jpg"
+              alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;" />
+            </div>
+            <div class="col-md-5 col-lg-7 d-flex align-items-center">
+              <div class="card-body p-lg-5 text-black">
+            
+                         <div class="card-body">
+                <h2 class="fw-bold pb-2 text-uppercase">Register</h2>
+            </div>
+            <form method="POST" action="">
+                                <label for="email"> <b>Email</b></label>
+                                <input type="text" class="form-control" id="email" name="email" placeholder="Masukan Alamat E-mail" required>
+                                <br>
+                                <label for="nama"> <b>Nama</b></label>
+                                <input type="text" class="form-control" id="nama" name="nama" placeholder="Masukan Nama Lengkap" required>
+                                <br>
+                                <label for="no_hp"> <b>Nomor Handphone</b></label>
+                                <input type="text" class="form-control" id="no_hp" name="no_hp" placeholder="Masukkan Nomor Handphone" required>
+                                <br>
+                                <label for="password"> <b>Kata Sandi</b></label>
+                                <input type="password" class="form-control" id="password" name="password" placeholder="Kata Sandi Anda" required>
+                                <br>
+                                <label for="password2"> <b>Komfirmasi Kata Sandi</b></label>
+                                <input type="password" class="form-control" id="password2" name="password2" placeholder="Konfirmasi Kata Sandi Anda" required>
+                                <br>
+                                <br>
+                                <center><button type="submit" name="daftar" class="btn btn-primary" style="width: 140px;">Daftar</button></center>
+                                <br>
+                                <p class="text-center">Anda sudah punya akun? <a href="Pradika_login.php">Login</a></p>
+                            </form>       
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+  <!-- End Card Login  -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
